47fc6da5f7e7cee1180d85f6d1b1b7d1fea2453b
Liu Rui <lovearuis@gmail.com>
2018-05-17 08:39:38 +0800

    优化页面跳转逻辑，缩短填报日志周期，启用无UIfirefox，全后台操作

--- src/main/groovy/com/aruistar/autocheckin/Application.groovy
+++ src/main/groovy/com/aruistar/autocheckin/Application.groovy
@@ -2,7 +2,9 @@ package com.aruistar.autocheckin
 
 import geb.Browser
 import geb.Page
+import org.openqa.selenium.firefox.FirefoxBinary
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 
 class Application {
     static void main(String[] args) {
@@ -13,24 +15,29 @@ class Application {
 
         //change it,and  you can download the driver from here https://github.com/mozilla/geckodriver/releases
         System.setProperty("webdriver.gecko.driver", "/Users/liurui/Documents/develop/webdriver/geckodriver")
+        System.setProperty("geb.build.baseUrl", "http://checkin2.longruan.com")
 
-        def browser = new Browser(driver: new FirefoxDriver())
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+
+//        def browser = new Browser(driver: new FirefoxDriver())
+        def browser = new Browser(driver: new FirefoxDriver(firefoxOptions))
 
         browser.with {
             to LoginPage
             username.value(USER_NAME)
             password.value(PASS_WORD)
             loginButton.click()
-            waitFor(10, 2) { rizhiguanli.click() }
-            waitFor(15, 2) { treeNodeLevel1.click() }
-            treeNodeLevel2.click()
+
+
+            to CheckinPage
 
             sleep(5 * 1000)
-            withFrame(myFrame) {
-                add.click()
-            }
+            add.click()
 
-            sleep(3 * 1000)
+            sleep(5 * 1000)
             textares.value("今天日志........................")
 
             submit.click()
@@ -41,8 +48,6 @@ class Application {
 }
 
 class LoginPage extends Page {
-    static url = "http://checkin2.longruan.com"
-
     static content = {
         loginButton(to: AdminPage) { $("#btnLogin") }
         username { $("input", name: "userName") }
@@ -50,24 +55,14 @@ class LoginPage extends Page {
     }
 }
 
-class AdminPage extends Page {
-    static content = {
 
-        rizhiguanli { $("a", text: "日志管理") }
-        treeNodeLevel1 { $("span", text: "日志管理") }
-        treeNodeLevel2 { $("span", text: "个人日志管理") }
+class CheckinPage extends Page {
 
+    static url = "/worklog/RZXX/GRRZ/Index"
 
-        myFrame(page: FrameDescribingPage) { $("iframe", name: "个人日志管理") }
-
-        textares { $("#WorkLog1") }
-        submit { $("span", text: "确定", class: "l-btn-text icon-ok l-btn-icon-left") }
-    }
-}
-
-class FrameDescribingPage extends Page {
     static content = {
         add { $("#link-Add") }
-
+        textares { $("#WorkLog1") }
+        submit { $("span", text: "确定", class: "l-btn-text icon-ok l-btn-icon-left") }
     }
 }
